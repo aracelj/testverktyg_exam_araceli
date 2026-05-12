ac48c7e26efdef025e6eb01e67d9dd2487963300
aracelj <aracelj10@gmail.com>
2026-05-12 10:59:22 +0200

    update maark favorite

--- src/features/environment.py
+++ src/features/environment.py
@@ -5,8 +5,8 @@ def before_scenario(context, scenario):
     context.browser = context.playwright.chromium.launch(headless=True)
     context.page = context.browser.new_page()
 
-    context.page.set_default_timeout(30000)
-    context.page.set_default_navigation_timeout(30000)
+    context.page.set_default_timeout(5000)
+    context.page.set_default_navigation_timeout(5000)
 
 def after_scenario(context, scenario):
     if hasattr(context, "browser"):
